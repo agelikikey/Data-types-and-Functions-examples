c206ca4a79df19894a2a8cf443500ef7aaf15ec6
agelikikey <agelikikey@gmail.com>
2020-01-30 00:00:05 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,62 @@
+// function myFunc() {
+//   let sum = 2 + 2;
+//   let arr = [1,2,3];
+//   arr[3] = sum;
+//   console.log(arr)
+// }
+
+// myFunc();
+// myFunc();
+// myFunc();
+
+//function names and parameters dont matter what you call them 
+// ex. 
+// function myFunc(param1) {
+//   let sum = 2 + 2;
+//   let arr = [1,2,3];
+//   arr[3] = sum;
+//   console.log(param1)
+// }
+// myFunc('string John');
+// myFunc(6);
+// myFunc([4,5,6]);
+
+// function sumNums(x,y) {
+//   console.log(x + y)
+// }
+// sumNums(5,5);
+// sumNums('john ', 'Smith');
+// sumNums(1,1);
+// sumNums(0,1);
+
+// function myFunc() {
+//   let sum = 2 + 2;
+//   let arr = [1,2,3];
+//   arr[3] = sum;
+//   return 'hello world'
+// }
+
+// // let x = myFunc();
+
+// // console.log(x)
+// console.log( myFunc());
+
+// function sumNums(x,y) {
+//   return x * y;
+// }
+
+// let var1 = sumNums(4,4);
+
+// console.log ( var1 );
+
+// function getItem(x,y) {
+//   let reslt = x[y];
+//   return reslt;
+// }
+
+// let arr = ['a','b','c','d'];
+
+// console.log( getItem(arr,3));
+
+
+// when you can identify the data type you can understand what the function is trying to do  
